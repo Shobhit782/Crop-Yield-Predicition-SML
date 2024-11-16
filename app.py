95219e49ed40a3ac275508000a83d78f635ba90e
Shobhit Choudhury <144991634+Shobhit782@users.noreply.github.com>
2024-11-16 23:20:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def predict():
         return render_template('index.html', prediction=predicted_yield_str)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
